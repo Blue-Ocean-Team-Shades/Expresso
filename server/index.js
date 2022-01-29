97125d25459b527f4f237a4ee7612e5078c26857
Kye <kyelindholm@gmail.com>
2022-01-29 17:33:44 -0500

    route for user to add coffe shops or drinks to favorites

--- server/index.js
+++ server/index.js
@@ -171,18 +171,23 @@ app.post('/shopratings', (req, res) => {
 app.post('/addfavorite', (req, res) => {
 
 
-  pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)} AND place_id = '${req.body.place_id}'`)
-    .then(data => {
-      if (data.rows.length > 0) { res.status(400).send("That's already a favorite of yours!") }
-      else {
-        if (req.body.isCoffee === 'true') {
+  if (req.body.isCoffee === 'true') {
+
+    pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)} AND drink_id = ${Number(req.body.drink_id)} AND place_id = '${req.body.place_id}'`)
+      .then(data => {
+        if (data.rows.length > 0) { res.status(400).send('This drink is already a favorite of yours!'); } else {
           pool.query(`INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (true, ${Number(req.body.user_id)}, '${req.body.place_id}', ${Number(req.body.drink_id)})`)
             .then(x => { res.status(200).send('Drink added to favorites!'); })
             .catch(err => {
               console.error(err);
               res.status(500).send();
             });
-        } else {
+        }
+      })
+  } else {
+    pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)} AND place_id = '${req.body.place_id}'`)
+      .then(data => {
+        if (data.rows.length > 0) { res.status(400).send('This shop is already a favorite of yours!'); } else {
           pool.query(`INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (false, ${Number(req.body.user_id)}, '${req.body.place_id}', null)`)
             .then(x => { res.status(200).send('Shop added to favorites!'); })
             .catch(err => {
@@ -190,13 +195,15 @@ app.post('/addfavorite', (req, res) => {
               res.status(500).send();
             });
         }
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send();
-    });
+      });
+  }
 });
+
+
+
+
+
+
 
 
 
