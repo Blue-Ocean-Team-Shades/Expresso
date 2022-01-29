3d282b1ea9982dc772cddb59dfef8911121aebdf
Liam Richardson <liam@richardsonconcepts.com>
2022-01-28 19:15:29 -0500

    Added Places API key to setup and config

--- setup.sh
+++ setup.sh
@@ -13,8 +13,10 @@ if [ ! -e "config.js" ]; then
   fi
   read -p "Postgres password (default no password): " -s password
   echo ""
+  read -p "Places API key: " placesKey
   echo "module.exports.host = '${host}';" > config.js
   echo "module.exports.user = '${user}';" >> config.js
   echo "module.exports.password = '${password}';" >> config.js
+  echo "module.exports.placesKey = '${placesApiKey}';" >> config.js
   echo "These choices can be changed any time by editing config.js (or deleting it and then re-running this script)"
 fi
