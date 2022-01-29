fae2aeafed59809422e508fbeb524a438eb86547
Emmanuel Garcia <egarcia5628@gmail.com>
2022-01-29 16:46:35 -0500

    created find shops function for client-side

--- server/models.js
+++ server/models.js
@@ -1,4 +1,6 @@
 const db = require('../database/index.js');
+const axios = require('axios');
+const config = require('../config.js');
 
 const getAll = (callback) => {
   let queryString = 'SELECT * FROM book_list_table_12'; // update query table
@@ -26,5 +28,18 @@ const create = (req, callback) => {
   });
 };
 
+// shops.data.results gives an array of the shop details.
+// On client-side, when called, create a callback to tell this function what to do with the data.
+const findShops = (query, callback) => {
+  axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${config.placesKey}`)
+    .then((shops) => {
+      callback(shops.data.results);
+    })
+    .catch((err) => {
+      callback(err)
+    })
+};
+
 module.exports.getAll = getAll;
 module.exports.create = create;
+module.exports.findShops = findShops;
