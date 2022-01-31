21a2d77d8749e668b3e14042119522b09dd83415
Liam Richardson <liam@richardsonconcepts.com>
2022-01-31 17:02:28 -0500

    Added geolocation, removed page timestamp

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,16 +6,52 @@ import LoginSignup from './login-and-signup';
 import ShopDetails from './shop-details';
 import TopBar from './top-bar';
 import { dummyShops } from '../dummyData.js';
+import axios from 'axios';
+
+function getLocation() {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position, err) => {
+        if (err) {
+          switch (err.code) {
+            case err.PERMISSION_DENIED:
+              console.error('location permissions denied');
+              break;
+            case err.POSITION_UNAVAILABLE:
+              console.error('position unavailable');
+              break;
+            case err.TIMEOUT:
+              console.error('location response timed out');
+              break;
+            case err.UNKNOWN_ERROR:
+              console.error('unknow location error');
+              break;
+          }
+          reject(null);
+        }
+        resolve(position.coords);
+      });
+    } else {
+      reject(null);
+    }
+  });
+}
 
 function App() {
-  //example persistent state
-  const [entryTime, setEntryTime] = useState(new Date().toString());
   const [shops, setShops] = useState(dummyShops);
   const [currentShop, setCurrentShop] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getLocation().then((position) => {
+      setLocation(position);
+      //TODO: enable this when server path is done
+      //axios.get('/search', { body: { location: `${location.latitude} ${location.longitude}` } });
+    });
+  }, []);
 
   return (
     <BrowserRouter>
-      Page loaded at: {entryTime}
       <TopBar />
       <Routes>
         <Route path='/' element={<ShopsList shops={shops} setCurrentShop={setCurrentShop} />} />
