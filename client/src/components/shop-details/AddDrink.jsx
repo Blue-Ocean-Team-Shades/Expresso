56628d96e709491651c66530b3c7e5c5c5745904
“Allen <allenalejandro3@gmail.com>
2022-01-31 23:36:01 -0500

    added toggle button

--- client/src/components/shop-details/AddDrink.jsx
+++ client/src/components/shop-details/AddDrink.jsx
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Input, FlexCol, AccentButton, FlexRow, Accent } from '../Styled.jsx';
 import axios from 'axios';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
-const Row = styled(FlexCol)`
+const Col = styled(FlexCol)`
   align-items: center;
-  justify-content: center;
+  // justify-content: center;
+`;
+
+const Row = styled(FlexRow)`
+  align-items: flex-end;
 `;
 
 const DrinkInput = styled(Accent)`
@@ -17,8 +23,20 @@ const DrinkName = styled(Input)`
   // background-color: #d1d1d1;
 `;
 
+// might add feature
 const DrinkPrice = styled(Input)`
   // background-color: #d1d1d1;
+`;
+
+const Recommend = styled(Accent)`
+  // background-color: #d1d1d1;
+`;
+
+const RecommendRow = styled(FlexRow)`
+  // border-radius: 5px;
+  // border: solid #c4c4c4 1px;
+  justify-content: center;
+  align-items: center;
 `;
 
 const AddDrinkButton = styled(AccentButton)`
@@ -30,6 +48,11 @@ let placeholder = `Don't see a drink here? Add it!`;
 function AddDrink({ currentShop, setCurrentShop }) {
   const [drinkValue, setDrinkValue] = useState('');
   const [priceValue, setPriceValue] = useState('');
+  const [alignment, setAlignment] = React.useState('yes');
+
+  const handleToggle = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   const handleDrinkInput = (e) => {
     setDrinkValue(e.target.value);
@@ -44,48 +67,107 @@ function AddDrink({ currentShop, setCurrentShop }) {
     setPriceValue('');
   };
 
-  // const addDrinkItem = () => {
-  //   let obj = {
-  //     place_id: 'ChIJr0p1HSe5QIYRJbI_fFPj6e0',
-  //     drink_name: 'Late',
-  //     recommend: true,
-  //   };
-  //   axios
-  //     .post('/drinkmenu', obj)
-  //     .then((result) => console.log(result))
-  //     .catch((err) => console.log(err));
-  // };
+  const addDrinkItem = () => {
+    let obj = {
+      place_id: 'ChIJr0p1HSe5QIYRJbI_fFPj6e0',
+      drink_name: 'Iced Coffee',
+      recommend: true,
+    };
+
+    axios
+      .post('/drinkmenu', obj)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  const getDrink = () => {
+    // let obj = {
+    //   place_id: 'ChIJr0p1HSe5QIYRJbI_fFPj6e0',
+    // };
+
+    let optionsConfig = {
+      params: {
+        place_id: 'ChIJr0p1HSe5QIYRJbI_fFPj6e0',
+      },
+    };
+
+    axios
+      .get('/drinkmenu', optionsConfig)
+      .then((data) => console.log(data.data))
+      .catch((err) => console.log(err));
+  };
 
   const clickHandler = (event) => {
     event.preventDefault();
     // add drink to database --> /drinkmenu
-    // // drink name , rating, placeid
+    // // placeid, drink name, recommend
     // setCurrentShop
-    // use current shop place id
-    // get current shop info from database
-    // setcurrentshop
+    // // use current shop place id
+    // // get current shop info from database
+    // // setcurrentshop
+    // addDrinkItem();
+    // options.body{obj}
+
+    getDrink();
     resetInputFields();
   };
 
   return (
-    <Row>
+    <Col>
       <DrinkInput>{placeholder}</DrinkInput>
-      <FlexRow>
-        <DrinkName
+      <Row>
+        <FlexCol>
+          <RecommendRow>
+            <DrinkName
+              placeholder='Drink Name'
+              value={drinkValue}
+              onChange={handleDrinkInput}
+            />
+          </RecommendRow>
+        </FlexCol>
+
+        <FlexCol>
+          <label>Recommend?</label>
+          <RecommendRow>
+            <ToggleButtonGroup
+              color='primary'
+              value={alignment}
+              exclusive
+              onChange={handleToggle}
+            >
+              <ToggleButton value='yes'>Yes</ToggleButton>
+              <ToggleButton value='no'>No</ToggleButton>
+            </ToggleButtonGroup>
+          </RecommendRow>
+        </FlexCol>
+        <AddDrinkButton onClick={clickHandler}>Add Drink</AddDrinkButton>
+      </Row>
+    </Col>
+  );
+}
+
+export default AddDrink;
+
+/* <DrinkName
           placeholder='Drink Name'
           value={drinkValue}
           onChange={handleDrinkInput}
-        />
-        <DrinkPrice
+        /> */
+/* <AddDrinkButton onClick={clickHandler}>Add Drink</AddDrinkButton> */
+
+/* <FlexCol>
+          <label>Recommend?</label>
+          <RecommendRow>
+            <AddDrinkButton onClick={clickHandler}>Yes</AddDrinkButton>
+            <AddDrinkButton onClick={clickHandler}>No</AddDrinkButton>
+          </RecommendRow>
+        </FlexCol> */
+
+/*
+ <DrinkPrice
           type='number'
           placeholder='Price'
           value={priceValue}
           onChange={handlePriceInput}
         />
-        <AddDrinkButton onClick={clickHandler}>Add Drink</AddDrinkButton>
-      </FlexRow>
-    </Row>
-  );
-}
-
-export default AddDrink;
+*/
--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const utils = require('./hashUtils.js');
 app.use(express.json());
 app.use(express.urlencoded());
 
-
 //janky fix, but it's fine because we're replacing all this with subdomains anyways
 const staticPath = '../client/dist';
 app.use('/', express.static(path.resolve(__dirname, staticPath)));
@@ -19,38 +18,47 @@ app.use('/login', express.static(path.resolve(__dirname, staticPath)));
 app.use('/signup', express.static(path.resolve(__dirname, staticPath)));
 app.use('/favorites', express.static(path.resolve(__dirname, staticPath)));
 
-
-
 /////////////* USER ACCOUNT ROUTES *////////////////
 
 //ROUTE FOR CREATING USER
 app.post('/signup', (req, res) => {
   //CHECKS IF USER WITH GIVEN USERNAME ALREADY EXISTS
-  pool.query(`SELECT * FROM users WHERE username = '${req.body.username}'`)
-    .then(data => {
+  pool
+    .query(`SELECT * FROM users WHERE username = '${req.body.username}'`)
+    .then((data) => {
       //IF NOT, CREATES USER, SALT, AND HASHED PASSWORD ->
       if (data.rows.length === 0 || data.rows === undefined) {
         let salt = utils.createRandom32String();
         let password = utils.createHash(req.body.password, salt);
 
         //STORES USERNAME, HASHED PWORD, AND SALT IN DB
-        pool.query(`INSERT INTO users (username, password, salt) VALUES ('${req.body.username}', '${password}', '${salt}')`)
-          .then(x => {
+        pool
+          .query(
+            `INSERT INTO users (username, password, salt) VALUES ('${req.body.username}', '${password}', '${salt}')`
+          )
+          .then((x) => {
             res.redirect(200, '/login');
           })
-          .catch(err => { throw err; });
+          .catch((err) => {
+            throw err;
+          });
       } else {
         //IF USER ALREADY EXISTS
-        res.status(500).send(`USER WITH USERNAME ${req.body.username} ALREADY EXISTS`);
+        res
+          .status(500)
+          .send(`USER WITH USERNAME ${req.body.username} ALREADY EXISTS`);
       }
     })
-    .catch(err => { throw err; });
+    .catch((err) => {
+      throw err;
+    });
 });
 
 //ROUTE FOR LOGGING IN
 app.post('/login', (req, res) => {
-  pool.query(`SELECT * FROM users WHERE username = '${req.body.username}'`)
-    .then(data => {
+  pool
+    .query(`SELECT * FROM users WHERE username = '${req.body.username}'`)
+    .then((data) => {
       let user = data.rows[0];
 
       //IF USERNAME IS NOT IN DATABASE -> 404/USER NOT FOUND
@@ -58,7 +66,7 @@ app.post('/login', (req, res) => {
         res.status(404).send('user not found');
       }
       //IF USERNAME IS IN DB AND PROVIDED PASSWORD HASHED WITH SALT RETURNED FROM QUERY MATCHES PASSWORD STORED IN DB
-      else if (utils.compareHash(req.body.password, user.password, user.salt)){
+      else if (utils.compareHash(req.body.password, user.password, user.salt)) {
         res.redirect(200, '/');
       } else {
         //IF PASSWORDS DON'T MATCH
@@ -67,9 +75,7 @@ app.post('/login', (req, res) => {
     });
 });
 
-
 ///////////////*DRINK MENU ROUTES*////////////////
-
 
 app.post('/drinkmenu', (req, res) => {
   let rating = 0;
@@ -77,14 +83,22 @@ app.post('/drinkmenu', (req, res) => {
 
   let drinkName = req.body.drink_name.toLowerCase();
 
-  pool.query(`SELECT * FROM drinks WHERE drink_name = '${drinkName}' AND place_id = '${req.body.place_id}'`)
-    .then(data => {
+  pool
+    .query(
+      `SELECT * FROM drinks WHERE drink_name = '${drinkName}' AND place_id = '${req.body.place_id}'`
+    )
+    .then((data) => {
       if (data.rows.length > 0) {
         res.status(400).send('Drink already exists in menu.');
       } else {
-        pool.query(`INSERT INTO drinks (drink_name, drink_rating, place_id) VALUES ('${drinkName}', ${rating}, '${req.body.place_id}')`)
-          .then(x => { res.status(200).send('Drink added!'); })
-          .catch(err => {
+        pool
+          .query(
+            `INSERT INTO drinks (drink_name, drink_rating, place_id) VALUES ('${drinkName}', ${rating}, '${req.body.place_id}')`
+          )
+          .then((x) => {
+            res.status(200).send('Drink added!');
+          })
+          .catch((err) => {
             res.status(500).send();
             console.error(err);
           });
@@ -94,20 +108,30 @@ app.post('/drinkmenu', (req, res) => {
 
 app.post('/drinkrating', (req, res) => {
   if (req.body.rating === '1') {
-    pool.query(`UPDATE drinks SET drink_rating = drink_rating + 1 WHERE id = ${Number(req.body.drink_id)}`)
-      .then(x => {
-        res.status(200).send('Drink rating updated +1!')
+    pool
+      .query(
+        `UPDATE drinks SET drink_rating = drink_rating + 1 WHERE id = ${Number(
+          req.body.drink_id
+        )}`
+      )
+      .then((x) => {
+        res.status(200).send('Drink rating updated +1!');
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).send();
         console.error(err);
-       });
+      });
   } else {
-    pool.query(`UPDATE drinks SET drink_rating = drink_rating - 1 WHERE id = ${Number(req.body.drink_id)}`)
-      .then(x => {
+    pool
+      .query(
+        `UPDATE drinks SET drink_rating = drink_rating - 1 WHERE id = ${Number(
+          req.body.drink_id
+        )}`
+      )
+      .then((x) => {
         res.status(200).send('Drink rating updated -1!');
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).send();
         console.error(err);
       });
@@ -115,49 +139,73 @@ app.post('/drinkrating', (req, res) => {
 });
 
 app.get('/drinkmenu', (req, res) => {
-  pool.query(`SELECT * FROM drinks WHERE place_id = '${req.body.place_id}'`)
-    .then(data => {
+  pool
+    .query(`SELECT * FROM drinks WHERE place_id = '${req.query.place_id}'`)
+    .then((data) => {
       res.status(200).send(data.rows);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
       res.status(500).send(err);
-    })
-})
+    });
+});
 
 //////////////*SHOP RATING ROUTEs*//////////////
 
 app.post('/shopratings', (req, res) => {
-  pool.query(`SELECT shop_rating FROM shops WHERE place_id = '${req.body.place_id}'`)
-    .then(data => {
+  pool
+    .query(
+      `SELECT shop_rating FROM shops WHERE place_id = '${req.body.place_id}'`
+    )
+    .then((data) => {
       if (data.rows.length > 0) {
         if (req.body.rating === '1') {
-          pool.query(`UPDATE shops SET shop_rating = shop_rating + 1 WHERE place_id = '${req.body.place_id}'`)
-            .then(x => { res.status(200).send('Review updated +1!'); })
-            .catch(err => {
+          pool
+            .query(
+              `UPDATE shops SET shop_rating = shop_rating + 1 WHERE place_id = '${req.body.place_id}'`
+            )
+            .then((x) => {
+              res.status(200).send('Review updated +1!');
+            })
+            .catch((err) => {
               res.status(500).send();
               console.error(err);
             });
         } else {
-          pool.query(`UPDATE shops SET shop_rating = shop_rating - 1 WHERE place_id = '${req.body.place_id}'`)
-            .then(x => { res.status(200).send('Review updated -1!'); })
-            .catch(err => {
+          pool
+            .query(
+              `UPDATE shops SET shop_rating = shop_rating - 1 WHERE place_id = '${req.body.place_id}'`
+            )
+            .then((x) => {
+              res.status(200).send('Review updated -1!');
+            })
+            .catch((err) => {
               res.status(500).send();
               console.error(err);
             });
         }
       } else {
         if (req.body.rating === '1') {
-          pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 1)`)
-            .then(x => { res.status(200).send('Shop rating added!'); })
-            .catch(err => {
+          pool
+            .query(
+              `INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 1)`
+            )
+            .then((x) => {
+              res.status(200).send('Shop rating added!');
+            })
+            .catch((err) => {
               res.status(500).send();
               console.error(err);
             });
         } else {
-          pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 0)`)
-            .then(x => { res.status(200).send('Shop rating added!'); })
-            .catch(err => {
+          pool
+            .query(
+              `INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 0)`
+            )
+            .then((x) => {
+              res.status(200).send('Shop rating added!');
+            })
+            .catch((err) => {
               res.status(500).send();
               console.error(err);
             });
@@ -167,13 +215,17 @@ app.post('/shopratings', (req, res) => {
 });
 
 app.get('/shopratings', (req, res) => {
-  let queryArg = req.body.shops.replace(/, /g, "', '").replace('[', "('").replace(']', "')");
+  let queryArg = req.body.shops
+    .replace(/, /g, "', '")
+    .replace('[', "('")
+    .replace(']', "')");
 
-  pool.query(`SELECT * FROM shops WHERE place_id IN ${queryArg}`)
-    .then(data => {
+  pool
+    .query(`SELECT * FROM shops WHERE place_id IN ${queryArg}`)
+    .then((data) => {
       res.status(200).send(data.rows);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
       res.status(500).send();
     });
@@ -183,24 +235,54 @@ app.get('/shopratings', (req, res) => {
 
 app.post('/favorites', (req, res) => {
   if (req.body.isCoffee === 'true') {
-    pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)} AND drink_id = ${Number(req.body.drink_id)} AND place_id = '${req.body.place_id}'`)
-      .then(data => {
-        if (data.rows.length > 0) { res.status(400).send('This drink is already a favorite of yours!'); } else {
-          pool.query(`INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (true, ${Number(req.body.user_id)}, '${req.body.place_id}', ${Number(req.body.drink_id)})`)
-            .then(x => { res.status(200).send('Drink added to favorites!'); })
-            .catch(err => {
+    pool
+      .query(
+        `SELECT * FROM favorites WHERE user_id = ${Number(
+          req.body.user_id
+        )} AND drink_id = ${Number(req.body.drink_id)} AND place_id = '${
+          req.body.place_id
+        }'`
+      )
+      .then((data) => {
+        if (data.rows.length > 0) {
+          res.status(400).send('This drink is already a favorite of yours!');
+        } else {
+          pool
+            .query(
+              `INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (true, ${Number(
+                req.body.user_id
+              )}, '${req.body.place_id}', ${Number(req.body.drink_id)})`
+            )
+            .then((x) => {
+              res.status(200).send('Drink added to favorites!');
+            })
+            .catch((err) => {
               console.error(err);
               res.status(500).send();
             });
         }
-      })
+      });
   } else {
-    pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)} AND place_id = '${req.body.place_id}'`)
-      .then(data => {
-        if (data.rows.length > 0) { res.status(400).send('This shop is already a favorite of yours!'); } else {
-          pool.query(`INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (false, ${Number(req.body.user_id)}, '${req.body.place_id}', null)`)
-            .then(x => { res.status(200).send('Shop added to favorites!'); })
-            .catch(err => {
+    pool
+      .query(
+        `SELECT * FROM favorites WHERE user_id = ${Number(
+          req.body.user_id
+        )} AND place_id = '${req.body.place_id}'`
+      )
+      .then((data) => {
+        if (data.rows.length > 0) {
+          res.status(400).send('This shop is already a favorite of yours!');
+        } else {
+          pool
+            .query(
+              `INSERT INTO favorites (isCoffee, user_id, place_id, drink_id) VALUES (false, ${Number(
+                req.body.user_id
+              )}, '${req.body.place_id}', null)`
+            )
+            .then((x) => {
+              res.status(200).send('Shop added to favorites!');
+            })
+            .catch((err) => {
               console.error(err);
               res.status(500).send();
             });
@@ -209,32 +291,32 @@ app.post('/favorites', (req, res) => {
   }
 });
 
-
-
 app.get('/userfavorites', (req, res) => {
   let returnObject = {
     favoriteDrinks: [],
-    favoriteShops: []
+    favoriteShops: [],
   };
 
-
-  pool.query(`SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)}`)
-    .then(data => {
+  pool
+    .query(
+      `SELECT * FROM favorites WHERE user_id = ${Number(req.body.user_id)}`
+    )
+    .then((data) => {
       for (let favorite of data.rows) {
-        if (favorite['iscoffee']) { returnObject.favoriteDrinks.push(favorite) } else {
+        if (favorite['iscoffee']) {
+          returnObject.favoriteDrinks.push(favorite);
+        } else {
           returnObject.favoriteShops.push(favorite);
         }
       }
 
       res.status(200).send(returnObject);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
       res.status(500).send();
     });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
