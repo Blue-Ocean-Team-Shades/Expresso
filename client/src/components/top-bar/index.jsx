ddb8130054ccc212a4a458bf5494eebc95892f14
Liam Richardson <liam@richardsonconcepts.com>
2022-02-01 12:59:37 -0500

    Added pop-down location search

--- client/src/components/top-bar/index.jsx
+++ client/src/components/top-bar/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Input } from '../Styled.jsx';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Options from './Options.jsx';
 import styled from 'styled-components';
-import Button from '@mui/material/Button';
+import { Button, Collapse, FormControl } from '@mui/material';
 import logo from '../../assets/logo.svg';
-import { colors, HighlightButton } from '../Styled.jsx';
+import { Input, colors, HighlightButton } from '../Styled.jsx';
 
 const FillSpace = styled.div`
   flex: 1;
@@ -23,9 +22,7 @@ const LogoButton = styled(HighlightButton)`
     transform-origin: top left;
     margin: 4px;
     border-radius: 50%;
-    box-shadow:
-                0 0px 3px #0003,
-                0 1px 2px #0003;
+    box-shadow: 0 0px 3px #0003, 0 1px 2px #0003;
   }
   & img {
     height: 100%;
@@ -40,16 +37,32 @@ const TitleBar = styled.div`
   z-index: 100;
 `;
 
+const OverlayCollapse = styled(Collapse)`
+  position: absolute;
+  left: 0;
+  top: 100%;
+  background-color: ${colors.mainLight}
+`
+
 function TopBar(props) {
   const location = useLocation();
   const navigate = useNavigate();
+  const [focused, setFocused] = useState(undefined);
+
   return (
     <TitleBar>
-      <LogoButton onClick={() => navigate('/')} disableRipple={true} >
+      <LogoButton onClick={() => navigate('/')} disableRipple={true}>
         <img src={logo} />
       </LogoButton>
       <FillSpace />
-      <Input label='search' size='small'/>
+      <form>
+        <FormControl onFocus={(e) => setFocused(e.target)} onBlur={() => setFocused(undefined)}>
+          <Input label='search for' size='small' style={{ zIndex: 10 }} />
+          <OverlayCollapse in={focused}>
+            <Input label='at location' placeholder='near me' size='small' style={{ zIndex: 10 }} />
+          </OverlayCollapse>
+        </FormControl>
+      </form>
       <Options />
     </TitleBar>
   );
