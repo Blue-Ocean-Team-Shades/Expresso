d2d75ed8e8f9117e55e160d14c570e76f4e16334
Emmanuel Garcia <egarcia5628@gmail.com>
2022-01-31 20:33:22 -0500

    Made get requests to get shop info and images

--- server/controllers.js
+++ server/controllers.js
@@ -20,5 +20,30 @@ const post = (req, res) => {
   });
 };
 
+const listsOfShops = async (query, location, callback) => {
+
+  // query is the string typed by the user on the client side.
+  // location should be an object with lat/lng coordinates | example {lat: 23.3453, lng:10.7546}
+  let queryString = query.split(' ').join('+');
+  let locationQuery = `${location.lat}%2C${location.lng}`;
+
+  let shops = await models.findShops(queryString, locationQuery);
+
+  return shops
+
+};
+
+const getShopImage = async (shop) => {
+
+  let reference = shop.photos[0].photo_reference;
+  let imageURL = await models.getPhotosOfShops(reference);
+
+  return imageURL
+
+}
+
+
 module.exports.get = get;
 module.exports.post = post;
+module.exports.listsOfShops = listsOfShops;
+module.exports.getShopImage = getShopImage;
