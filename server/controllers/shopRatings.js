369b460c1390970c1a4e04a662ce5ef29bd177a1
Jared Maldonado <jrmaldo1228@gmail.com>
2022-02-01 14:46:01 -0600

    Users can now only rate shops when they are logged in

--- server/controllers/shopRatings.js
+++ server/controllers/shopRatings.js
@@ -2,42 +2,47 @@ const { pool } = require("../../database");
 
 
 const addShopRating = (req, res) => {
-  pool.query(`SELECT shop_rating FROM shops WHERE place_id = '${req.body.place_id}'`)
-    .then(data => {
-      if (data.rows.length > 0) {
-        if (req.body.rating === '1') {
-          pool.query(`UPDATE shops SET shop_rating = shop_rating + 1 WHERE place_id = '${req.body.place_id}'`)
-            .then(x => { res.status(200).send('Review updated +1!'); })
-            .catch(err => {
-              res.status(500).send();
-              console.error(err);
-            });
+  if (req.session.isLoggedIn) {
+    pool.query(`SELECT shop_rating FROM shops WHERE place_id = '${req.body.place_id}'`)
+      .then(data => {
+        if (data.rows.length > 0) {
+          if (req.body.rating === '1') {
+            pool.query(`UPDATE shops SET shop_rating = shop_rating + 1 WHERE place_id = '${req.body.place_id}'`)
+              .then(x => { res.status(200).send('Review updated +1!'); })
+              .catch(err => {
+                res.status(500).send();
+                console.error(err);
+              });
+          } else {
+            pool.query(`UPDATE shops SET shop_rating = shop_rating - 1 WHERE place_id = '${req.body.place_id}'`)
+              .then(x => { res.status(200).send('Review updated -1!'); })
+              .catch(err => {
+                res.status(500).send();
+                console.error(err);
+              });
+          }
         } else {
-          pool.query(`UPDATE shops SET shop_rating = shop_rating - 1 WHERE place_id = '${req.body.place_id}'`)
-            .then(x => { res.status(200).send('Review updated -1!'); })
-            .catch(err => {
-              res.status(500).send();
-              console.error(err);
-            });
+          if (req.body.rating === '1') {
+            pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 1)`)
+              .then(x => { res.status(200).send('Shop rating added!'); })
+              .catch(err => {
+                res.status(500).send();
+                console.error(err);
+              });
+          } else {
+            pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 0)`)
+              .then(x => { res.status(200).send('Shop rating added!'); })
+              .catch(err => {
+                res.status(500).send();
+                console.error(err);
+              });
+          }
         }
-      } else {
-        if (req.body.rating === '1') {
-          pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 1)`)
-            .then(x => { res.status(200).send('Shop rating added!'); })
-            .catch(err => {
-              res.status(500).send();
-              console.error(err);
-            });
-        } else {
-          pool.query(`INSERT INTO shops (place_id, shop_rating) VALUES ('${req.body.place_id}', 0)`)
-            .then(x => { res.status(200).send('Shop rating added!'); })
-            .catch(err => {
-              res.status(500).send();
-              console.error(err);
-            });
-        }
-      }
-    });
+      });
+  } else {
+    alert('Please login to rate coffee shops');
+    res.redirect('/login');
+  }
 };
 
 const getShopRatings = (req, res) => {
