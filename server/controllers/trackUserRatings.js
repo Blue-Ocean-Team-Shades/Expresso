af1cd1ed808880e0d6d6b86fba98ff7f099e0779
Jared Maldonado <jrmaldo1228@gmail.com>
2022-02-03 10:58:52 -0600

    Refactored userRatedShop to async/await

--- server/controllers/trackUserRatings.js
+++ server/controllers/trackUserRatings.js
@@ -1,51 +1,39 @@
 const { pool } = require('../../database');
 
-const userRatedShop = (place_id, user_id, isUpvote) => {
-  /*
-    make alreadyVoted variable and set to false
-    query user_shop_votes table to find a row where place_id = inputed place_id
-    and user_id = user_id from current session
-      if it doesn't exist create it with the default values
-      if isUpvote is true
-        if user hasn't already upvoted
-          update row where user_id and place_id = inputed values so that
-          upvote is true and downvote is false
-        else set alreadyVoted to true and return it
-      else
-        if user hasn't already downvoted
-          update row where user_id and place_id = inputed values so that
-          upvote is false and downvote is true
-        else set alreadyVoted to true and return it
-    return alreadyVoted
-  */
-  let alreadyVoted = false;
-  pool.query(`SELECT * FROM user_rated_shop WHERE place_id = ${place_id} AND user_id = ${user_id}`)
-    .then(data => {
-      console.log(data.rows[0]);
-      if (data.rows.length === 0) {
-        pool.query(`INSERT INTO user_rated_shop(place_id, user_id) VALUES (${place_id}, ${user_id})`);
-      }
-      return data;
-    })
-    .then(data => {
-      if (isUpvote) {
-        if (!data.rows[0].upvoted) {
-          pool.query(`UPDATE user_rated_shop SET upvoted = ${true}, downvoted = ${false} WHERE place_id = ${place_id} AND user_id = ${user_id}`);
-        } else {
-          alreadyVoted = true;
-        }
+const userRatedShop = async (place_id, user_id, isUpvote) => {
+  let userVote;
+
+  try {
+    userVote = await pool.query(`SELECT * FROM user_rated_shop WHERE place_id = ${place_id} AND user_id = ${user_id};`);
+  } catch (err) {
+    console.error('Error getting user_rated_shops: ', err);
+  }
+
+  try {
+    if (userVote.rows[0].length === 0) {
+      await pool.query(`INSERT INTO user_rated_shop(place_id, user_id) VALUES (${place_id}, ${user_id});`);
+    };
+  } catch (err) {
+    console.error('Error inserting new shop rating into user_rated_shops: ', err);
+  }
+
+  try {
+    if (isUpvote) {
+      if(!userVote.rows[0].upvoted) {
+        await pool.query(`UPDATE user_rated_shop SET upvoted = true, downvoted = false WHERE place_id = ${place_id} AND user_id = ${user_id};`);
       } else {
-        if (!data.rows[0].downvoted) {
-          pool.query(`UPDATE user_rated_shop SET upvoted = ${false}, downvoted = ${true} WHERE place_id = ${place_id} AND user_id = ${user_id}`);
-        } else {
-          alreadyVoted = true;
-        }
+        return true;
       }
-    })
-    .then(() => {
-      return alreadyVoted;
-    })
-    .catch(err => console.log('Error updating user\'s shop vote', err));
+    } else if (!isUpvote) {
+      if (!userVote.rows[0].downvoted) {
+        await pool.query(`UPDATE user_rated_shop SET upvoted = false, downvoted = true WHERE place_id = ${place_id} AND user_id = ${user_id};`);
+      } else {
+        return true;
+      }
+    }
+  } catch (err) {
+    console.error('Error updating user_rated_shops: ', err);
+  }
 }
 
 const userRatedCoffee = async (drink_id, user_id, isUpvote) => {
