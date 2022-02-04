e3fa47289d7785d3597d0223359b389750e1d5fd
Liam Richardson <liam@richardsonconcepts.com>
2022-02-04 13:05:47 -0500

    DRY-er

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import Styled, {isMobile} from './Styled.jsx';
+import Styled, { isMobile } from './Styled.jsx';
 import styled from 'styled-components';
 import React, { useState, useEffect, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -55,46 +55,43 @@ function App() {
   const [mobile, setMobile] = useState(false);
 
   useEffect(() => {
-    setMobile(isMobile())
+    setMobile(isMobile());
     window.addEventListener('resize', () => {
-      setMobile(isMobile())
-    })
-  }, [])
+      setMobile(isMobile());
+    });
+  }, []);
 
   useEffect(() => {
-    const newCookies = updateCookies()
+    const newCookies = updateCookies();
     //hacky redundancy
     if (cookies.length === 0) {
-      if (newCookies.location) {
-        //TODO: periodically check for new location
-        const position = JSON.parse(newCookies.location)
-        setMessage(null);
-        setLocation(position)
-        api.getShops(position)
-          .then(({data}) => setShops(data))
-          .catch(err => {
-            console.error(err)
-            setMessage('Unable to get shops at location')
-          })
-      } else {
-        setMessage('Fetching location');
-        getLocation()
-          .then((position) => {
+      new Promise((resolve, reject) => {
+        if (newCookies.location) {
+          //TODO: periodically check for new location
+          const position = JSON.parse(newCookies.location);
+          setMessage(null);
+          setLocation(position);
+          resolve(position);
+        } else {
+          setMessage('Fetching location');
+          return getLocation().then((position) => {
             setMessage(null);
             setLocation(position);
-            const positionOb = {latitude: position.latitude, longitude: position.longitude}
+            const positionOb = { latitude: position.latitude, longitude: position.longitude };
             document.cookie = `location=${JSON.stringify(positionOb)}; path=/`;
-            return api.getShops(position);
-          })
-          .then(({ data }) => {
-            setShops(data);
-          })
-          .catch((err) => {
-            //TODO: catch separate error for location services earlier
-            console.error(err);
-            setMessage('Please enable location, or enter a location in the search!');
+            resolve(position);
           });
-      }
+        }
+      })
+        .then((position) => api.getShops(position))
+        .then(({ data }) => {
+          setShops(data);
+        })
+        .catch((err) => {
+          //TODO: catch separate error for location services earlier
+          console.error(err);
+          setMessage('Please enable location, or enter a location in the search!');
+        });
     }
   }, [document.cookie]);
 
@@ -142,13 +139,13 @@ function App() {
             });
             return api.getUserFavorites(response.user_id);
           })
-          .then(({data}) => {
+          .then(({ data }) => {
             const newFavoriteShops = {};
             for (const shop of data.favoriteShops) {
               newFavoriteShops[shop.place_id] = true;
             }
             setFavoriteShops(newFavoriteShops);
-            const newFavoriteDrinks = {}
+            const newFavoriteDrinks = {};
             for (const drink of data.favoriteDrinks) {
               if (!newFavoriteDrinks[drink.place_id]) newFavoriteDrinks[drink.place_id] = {};
               newFavoriteDrinks[drink.place_id][drink.drink_id] = true;
@@ -159,13 +156,13 @@ function App() {
       }
     }
     setCookies(newCookies);
-    return newCookies
+    return newCookies;
   }
 
   return (
     <BrowserRouter>
-      <div style={{ height: '100vh', width:'100vw', maxHeight: '100vh', display: 'flex' }}>
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+      <div style={{ height: '100vh', width: '100vw', maxHeight: '100vh', display: 'flex' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
           <TopBar
             searchTerm={searchTerm}
             setSearchTerm={setSearchTerm}
@@ -176,7 +173,7 @@ function App() {
             updateCookies={updateCookies}
             isLoggedIn={isLoggedIn}
           />
-          <div style={{flex: 1, overflow:'auto'}}>
+          <div style={{ flex: 1, overflow: 'auto' }}>
             <Routes>
               <Route
                 path='/'
@@ -212,7 +209,9 @@ function App() {
               />
               <Route
                 path='/login'
-                element={<LoginSignup isLogin={true} cookies={cookies} updateCookies={updateCookies} />}
+                element={
+                  <LoginSignup isLogin={true} cookies={cookies} updateCookies={updateCookies} />
+                }
               />
               <Route
                 path='/signup'
